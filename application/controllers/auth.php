a941ac898035a5eaf864579e3039abf840568c60
Rufus Mbugua <mbuguarufus@gmail.com>
2012-04-30 12:46:57 +0300

    Changes

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -1,36 +1,34 @@
 <?php
 /*helps authenticate a user*/
 class Auth extends MY_Controller {
-	
+
 	public function __construct() {
 		parent::__construct();
-		
+
 	}
 
 	public function login() {
-		
+
 		//working with a user defined query
 		/*$query = $this->em->createQuery('SELECT u.id,u.username,u.access_level FROM models\SystemUser u WHERE u.username = :name AND u.password= :secret');
-          $query->setParameter('name', $this -> input -> post('username'));
-		  $query->setParameter('secret', $this -> input -> post('secret'));
-          $user = $query->getResult(); */
-       
-       //Working with an object of the entity
-		$user = $this->em->getRepository('models\SystemUser')
-						->findOneBy(array('username' => $this -> input -> post('username'), 'password' => $this -> input -> post('secret')));
-     
-			
+		 $query->setParameter('name', $this -> input -> post('username'));
+		 $query->setParameter('secret', $this -> input -> post('secret'));
+		 $user = $query->getResult(); */
+
+		//Working with an object of the entity
+		$user = $this -> em -> getRepository('models\SystemUser') -> findOneBy(array('userName' => $this -> input -> post('username'), 'userPassword' => $this -> input -> post('secret')));
+
 		//if ($user[0]['id'] >0) {
-	    if ($user) {
+		if ($user) {
 			//$user = $this->em->find('models\SystemUser',$user[0]['id']); //pass to the object
 
-			$email = $user -> getUsername();
-			$userRights=$user->getAccessLevel();
+			$email = $user -> getUserName();
+			$userRights = $user -> getUserRights();
 
 			$newdata = array('email' => $email, 'logged_in' => TRUE);
 
 			$this -> session -> set_userdata($newdata);
-			
+
 			/* Check Authority / User Level
 			 * Where:
 			 * 1. Administrator  =>  HIGHEST
@@ -49,7 +47,6 @@ class Auth extends MY_Controller {
 					break;
 			}
 
-
 		} else {
 			#use an ajax request and not a whole refresh
 			$data['form'] = '<p>User Not Found!<p>';
@@ -57,14 +54,13 @@ class Auth extends MY_Controller {
 		}
 
 	}
-	
-	
-	
-	public function logout(){
+
+	public function logout() {
 		$data['form'] = '<p>You need to login.<p>';
 		$this -> load -> view('index', $data);
-		$this->session->sess_destroy();
+		$this -> session -> sess_destroy();
 		redirect(base_url(), 'refresh');
 	}
 
-}?>
+}
+?>
